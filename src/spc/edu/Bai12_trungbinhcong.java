27c314132201d8176cc75faca6a4864dcc89ed13
TACNTH <letuananhnguyen70@gmail.com>
2025-09-16 10:22:37 +0700

    them

---
+++ src/spc/edu/Bai12_trungbinhcong.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai12_trungbinhcong {
+    public static void main(String[] args) {
+         Scanner nhap = new Scanner(System.in);
+         
+         System.out.println("nhap con so: ");
+          int ConSo = nhap.nextInt();
+           System.out.println( "nhap mu: ");
+            int SoMu = nhap.nextInt();
+            long ketqua = 1 ;
+    for (int i = 0; i < SoMu; i++)
+    {
+       ketqua *= ConSo;
+    }
+        System.out.println(ConSo +"^" + SoMu + "=" + ketqua);
+        nhap.close();
+    }
+    
+}
+
